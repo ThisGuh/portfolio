6108e49878cd6e356d8eb0f02bbb574641330a25
thisGuh <jakiepolska1111@gmail.com>
2021-05-02 17:37:51 +0200

    chore: fix absolute import

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   plugins: [
     {
@@ -6,6 +8,14 @@ module.exports = {
         isTSX: true, // defaults to false
         jsxPragma: `jsx`, // defaults to "React"
         allExtensions: true, // defaults to false
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        src: path.join(__dirname, 'src'),
+        pages: path.join(__dirname, 'src/pages'),
+        components: path.join(__dirname, 'src/components'),
       },
     },
   ],
