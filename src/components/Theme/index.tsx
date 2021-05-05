83b817d80a6403bb8fd1fd2fc3c6d854a30bab96
thisGuh <jakiepolska1111@gmail.com>
2021-05-05 20:06:37 +0200

    style: add global style and normalize css

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 import { defaultTheme } from 'utility/theme'
+import { normalize } from 'utility/normalize'
 
 interface Props {
   children?: React.ReactNode
 }
 
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+`
+
 function Theme({ children }: Props) {
-  return <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      {children}
+    </ThemeProvider>
+  )
 }
 
 export default Theme
