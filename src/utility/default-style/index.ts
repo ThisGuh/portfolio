5557db34bbdfa3fc3e64aa761f3aa12c8f4ef2a7
thisGuh <jakiepolska1111@gmail.com>
2021-05-08 20:55:29 +0200

    style: add custom font

--- src/utility/default-style/index.ts
+++ src/utility/default-style/index.ts
@@ -3,6 +3,7 @@ import { css } from 'styled-components'
 export const defaultStyle = css`
   html {
     font-size: 10px;
+    font-family: 'poppins', sans-serif;
     box-sizing: border-box;
   }
 `
