28d60d16a03bccbb66dd11ec358f9e97caf8d43a
thisGuh <jakiepolska1111@gmail.com>
2021-05-09 20:26:54 +0200

    chore: add button component

---
+++ src/components/Button/Button.style.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const StyledButton = styled.button`
+  width: 100px;
+  height: 40px;
+  padding: 5px;
+  background-color: ${({ theme }) => theme.colors.lightPurple};
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.fontSizes.normal};
+  border: none;
+  cursor: pointer;
+
+  @media (${({ theme }) => theme.screenSizes.desktop}) {
+    width: 130px;
+    height: 50px;
+    padding: 8px;
+    margin: 15px;
+    font-size: ${({ theme }) => theme.fontSizes.medium};
+  }
+`
