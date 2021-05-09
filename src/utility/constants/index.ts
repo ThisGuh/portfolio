721398ca470d0c21d262f6a4bb8f7dbb43a4fcba
thisGuh <jakiepolska1111@gmail.com>
2021-05-09 20:24:10 +0200

    chore: update consts

--- src/utility/constants/index.ts
+++ src/utility/constants/index.ts
@@ -1,1 +1,7 @@
+//hero section
 export const HERO_H1_CONTENT = 'Damian Głuch'
+export const HERO_DESC = 'Front-end Developer'
+export const HERO_BUTTON_CONTENT = 'Kontakt'
+//links
+export const GITHUB_LINK = 'https://github.com/ThisGuh'
+export const LINKEDIN_LINK = 'https://www.linkedin.com/in/damian-gluch/'
