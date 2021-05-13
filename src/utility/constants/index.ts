cb21449a6fc7986686ec334d597de5c5fdae6417
thisGuh <jakiepolska1111@gmail.com>
2021-05-13 21:07:04 +0200

    chore: add about me description

--- src/utility/constants/index.ts
+++ src/utility/constants/index.ts
@@ -3,6 +3,13 @@ export const HERO_H1_CONTENT = 'Damian Głuch'
 export const HERO_DESC = 'Front-end Developer'
 export const HERO_BUTTON_CONTENT = 'Kontakt'
 export const HERO_NAV_CONTENT = ['O mnie', 'Projekty', 'Kontakt']
+//about section
+export const ABOUT_DESC = `
+    Cześć 👋
+    Swoją przygodę z programowaniem rozpocząłem w 2016 roku poznając C++.
+    Z czasem przeniosłem się na front-end rozwijając znajomość m.in JavaScript'u.
+    Dużą uwagę przykładam do ciągłego rozwoju, aby nie spocząć na laurach.
+`
 //links
 export const GITHUB_LINK = 'https://github.com/ThisGuh'
 export const LINKEDIN_LINK = 'https://www.linkedin.com/in/damian-gluch/'
