7bab4223259b909d5c2752e29baaa92b7a71b455
thisGuh <jakiepolska1111@gmail.com>
2021-06-02 14:34:02 +0200

    chore: improve props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,10 +3,17 @@ import { StyledButton } from 'components/Button/Button.style'
 
 interface Props {
   text: string
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
+  small?: boolean
+  type?: 'button' | 'submit' | 'reset' | undefined
 }
 
-function Button({ text }: Props) {
-  return <StyledButton>{text}</StyledButton>
+function Button({ text, small, onClick, type }: Props) {
+  return (
+    <StyledButton small={small} onClick={onClick} type={type}>
+      {text}
+    </StyledButton>
+  )
 }
 
 export default Button
